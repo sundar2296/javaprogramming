e880b68190e1ad1cfa2bf0a8adfe9d7dcd731dfb
sundar2296 <sundarbalaji96@gmail.com>
2020-07-28 15:09:20 +0530

    first commit....

---
+++ src/List/java/arrayList.java
@@ -0,0 +1,40 @@
+package List.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class arrayList {
+
+	public static void main(String[]args) {
+		
+		List<String>arraylist=new ArrayList<String>();
+		
+		arraylist.add("tata");
+		arraylist.add("Maruthi");
+		arraylist.add("Nexa");
+		arraylist.add("Kia");
+		arraylist.add("Hyundai");
+		
+		System.out.println(arraylist);
+		
+		//to find size
+		int size =arraylist.size();
+		System.out.println(arraylist.size());
+		
+		//to get particular value
+		String get =arraylist.get(3);
+		System.out.println(arraylist.get(3));
+		
+		//to find the index of the value
+		System.out.println(arraylist.lastIndexOf("hyundai"));
+		
+		//for loop
+		for(int i=0;i<=arraylist.size();i++) {
+			System.out.println(arraylist.get(i));
+			
+		}
+		
+		
+		
+	}
+}
---
+++ src/List/java/list.java
@@ -0,0 +1,21 @@
+package List.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class list {
+	
+	public static void main(String[]args) {
+		
+		List li =new ArrayList();
+		li.add(10);
+		li.add('M');
+		li.add("Sundar");
+		li.add(10.23);
+		
+		System.out.println(li);
+		
+		
+	}
+
+}
